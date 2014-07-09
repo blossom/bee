84752d9604fb860506c47a04c70f38600abe239c
Nik Graf <nik@blossom.io>
2014-07-09 15:03:57 +0200

    feat(loading): Added basic structure for loading component

--- build.dart
+++ build.dart
@@ -1,5 +1,7 @@
 import 'package:polymer/builder.dart';
 
 void main(List<String> args) {
- lint(entryPoints: ['example/button/index.html'], options: parseOptions(args));
+ var entryPoints = ['example/button/index.html',
+                    'example/loading/index.html'];
+ lint(entryPoints: entryPoints, options: parseOptions(args));
 }
---
+++ lib/components/loading/loading.dart
@@ -0,0 +1,9 @@
+import 'package:polymer/polymer.dart';
+
+@CustomTag('b-loading')
+class BeeLoading extends PolymerElement with Polymer {
+
+  BeeLoading.created() : super.created() {
+     polymerCreated();
+  }
+}
