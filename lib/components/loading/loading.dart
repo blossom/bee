5bf2d802cbe606f98e0c3dac0e314312f5aa7263
Nik Graf <nik@blossom.io>
2014-07-09 15:49:46 +0200

    feat(loading): Implemented loading component

--- lib/components/loading/loading.dart
+++ lib/components/loading/loading.dart
@@ -1,9 +1,29 @@
 import 'package:polymer/polymer.dart';
+import 'dart:html';
 
 @CustomTag('b-loading')
-class BeeLoading extends PolymerElement with Polymer {
+class BeeLoading extends PolymerElement {
+
+  @published String color;
+  String _defaultColor = "#505050";
 
   BeeLoading.created() : super.created() {
      polymerCreated();
+  }
+
+  void attached() {
+    // Setting the color to the default color in case no color
+    // has been provided through the color attribute.
+    if (color == null) {
+      updateColor(_defaultColor);
+    }
+  }
+
+  void colorChanged(String oldValue, String newValue) {
+    updateColor(newValue);
+  }
+
+  void updateColor(String value) {
+    shadowRoot.querySelector('.q-b-loading').style.color = value;
   }
 }
