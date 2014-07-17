2af7fc64101fe5919ffdab5064d914aeb0dfd14e
Patrick Wied <patrick.wied@live.at>
2014-07-17 14:55:35 +0200

    chore(secret): Add entrypoint

--- build.dart
+++ build.dart
@@ -1,5 +1,6 @@
 import 'package:polymer/builder.dart';
 
 void main(List<String> args) {
- lint(entryPoints: ['example/button/index.html'], options: parseOptions(args));
+ lint(entryPoints: ['example/button/index.html',
+                    'example/button/index.html'], options: parseOptions(args));
 }
