5406d0acf76405712c534e6970197adfda8ce6ac
Nik Graf <nik@blossom.io>
2014-08-06 10:38:53 +0200

    feat(editable text): add basic structure

--- build.dart
+++ build.dart
@@ -2,6 +2,7 @@ import 'package:polymer/builder.dart';
 
 void main(List<String> args) {
  var entryPoints = ['example/button/index.html',
+                    'example/editable_text/index.html',
                     'example/secret/index.html',
                     'example/loading/index.html',
                     'example/popover/index.html',
---
+++ lib/components/editable_text/editable_text.dart
@@ -0,0 +1,11 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+
+@CustomTag('b-editable-text')
+class BeeEditableText extends PolymerElement {
+
+  BeeEditableText.created() : super.created() {}
+
+  void attached() {
+  }
+}
